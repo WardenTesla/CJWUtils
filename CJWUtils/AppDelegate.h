cf44045efa9becf385e1105896728c2a1b4b7d27
frank <fk911c@gmail.com>
2014-10-09 13:51:28 +0800

    Initial Commit

---
+++ CJWUtils/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJWUtils
+//
+//  Created by cen on 9/10/14.
+//  Copyright (c) 2014 cen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

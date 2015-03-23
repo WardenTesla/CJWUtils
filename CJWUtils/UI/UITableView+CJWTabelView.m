35f34ebee8620a077bb1314c279ad2bdf346b854
frank <fk911c@gmail.com>
2015-03-23 22:07:04 +0800

    update

--- CJWUtils/UI/UITableView+CJWTabelView.m
+++ CJWUtils/UI/UITableView+CJWTabelView.m
@@ -20,4 +20,35 @@
     [self scrollToRowAtIndexPath:path atScrollPosition:UITableViewScrollPositionTop animated:animate];
 }
 
+
+/*
+ private func createLabel(txt:String) -> UILabel{
+ var label = UILabel(frame: CGRectMake(0, 0, self.frame.width, 44))
+ label.text = txt
+ label.textAlignment = NSTextAlignment.Center
+ label.textColor = UIColor.lightGrayColor()
+ return label
+ }
+ 
+ func showHeaderTxt(txt:NSString){
+ self.tableHeaderView = createLabel(txt)
+ }
+ 
+ func showFooterTxt(txt:NSString){
+ self.tableFooterView = createLabel(txt)
+ }
+ 
+ func hideHeaderTxt(){
+ self.tableHeaderView = UIView(frame: CGRectZero)
+ }
+ 
+ func hideFooterTxt(){
+ self.tableFooterView = UIView(frame: CGRectZero)
+ }
+ */
+
+-(void)hideFooterTxt{
+    self.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+}
+
 @end
